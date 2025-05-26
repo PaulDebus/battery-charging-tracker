5fe3420d1de46cc74b4ab46a68de2f9d6ffdcc8b
Paul Debus <PaulDebus@users.noreply.github.com>
2025-05-26 10:29:05 +0000

    try fix version shit

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 
 plugins {
     id("com.android.application") apply false
-    id("com.google.devtools.ksp") version "2.1.20-1.0.27" apply false
+    id("com.google.devtools.ksp") version "2.1.20-2.0.1" apply false
     id("com.android.library") apply false
     kotlin("android") apply false
     alias(libs.plugins.compose.compiler) apply false
